bbce168ae888595667def1285079a8aa538b2051
Franco <67444744+Francobeltramella@users.noreply.github.com>
2021-01-11 19:55:12 -0300

    Update UserController.php

--- LoginPHP/UserController.php
+++ LoginPHP/UserController.php
@@ -6,7 +6,7 @@
    $password = md5($_GET['pass']);
 
    //Validar datos para ingreso
-   if (($user == "elelog@elelog.es") AND ($password == "12345")) {
+   if (($user == "admin@admin.com") AND ($password == "12345")) {
         header('location:alerts.php?msj=Bienvenido&c=home&p=in&t=success');
     }else{
         header('location:alerts.php?msj=Usuario o contrasenia incorrectos&c=home&p=in&t=error');
